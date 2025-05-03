351564fc0486eb725c4475bbaaab23fb33a46716
Afif-Ghifari <aghifari1204@gmail.com>
2025-05-03 20:13:21 +0700

    fix setting

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -2,14 +2,14 @@
   'use strict';
   $(function() {
     $(".nav-settings").on("click", function() {
-      $("#right-sidebar").toggleClass("open");
+      $("#right-sidebar").addClass("open");
     });
     $(".settings-close").on("click", function() {
       $("#right-sidebar,#theme-settings").removeClass("open");
     });
 
     $("#settings-trigger").on("click" , function(){
-      $("#theme-settings").toggleClass("open");
+      $("#theme-settings").addClass("open");
     });
 
 
