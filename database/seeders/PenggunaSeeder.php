ca9d5363aa9c4213f44116d6d52ae4616bf3319c
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-05-05 17:28:52 +0700

    Refactor (#3)
    * refactor: rename 'hash_password' to 'password' in model, migration, and seeder

--- database/seeders/PenggunaSeeder.php
+++ database/seeders/PenggunaSeeder.php
@@ -18,37 +18,37 @@ class PenggunaSeeder extends Seeder
                 'id_peran' => 1,
                 'username' => 'admin',
                 'nama' => 'Natalia Willy',
-                'hash_password' => Hash::make('12345')
+                'password' => Hash::make('12345')
             ],
             [
                 'id_peran' => 2,
                 'username' => 'mahasiswa',
                 'nama' => 'Rizky Maulana',
-                'hash_password' => Hash::make('12345')
+                'password' => Hash::make('12345')
             ],
             [
                 'id_peran' => 3,
                 'username' => 'dosen',
                 'nama' => 'Dr. Andi Saputra',
-                'hash_password' => Hash::make('12345')
+                'password' => Hash::make('12345')
             ],
             [
                 'id_peran' => 4,
                 'username' => 'tendik',
                 'nama' => 'Budi Santoso',
-                'hash_password' => Hash::make('12345')
+                'password' => Hash::make('12345')
             ],
             [
                 'id_peran' => 5,
                 'username' => 'sarpras',
                 'nama' => 'Siti Aminah',
-                'hash_password' => Hash::make('12345')
+                'password' => Hash::make('12345')
             ],
             [
                 'id_peran' => 6,
                 'username' => 'teknisi',
                 'nama' => 'Joko Susilo',
-                'hash_password' => Hash::make('12345')
+                'password' => Hash::make('12345')
             ],
         ];
 
