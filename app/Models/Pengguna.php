35db3e7fc57735c8bbbd08e5b844b0acc337c38b
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-05-05 19:57:50 +0700

    Feat(authenticate): Menambahkan fitur registrasi dan login (#4)
    * feat(authenticate): implement authentication with registration and login functionality

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Pengguna extends Model
+class Pengguna extends Authenticatable
 {
     use HasFactory;
     protected $table = 'pengguna';
@@ -19,6 +20,15 @@ class Pengguna extends Model
         'nama',
         'password',
         'foto_profil',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
     ];
 
     public function peran(): BelongsTo
